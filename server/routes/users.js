1d9db9819cdae831b30cf24fc44f780e9173fb53
MinGyeongCHO <jmk5357@naver.com>
2022-03-21 07:05:05 +0000

    220321_cmg

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,8 @@
-// const express = require('express');
-// const router = express.Router();
-// const ctrl = require('../controllers/controllers');
+const express = require('express');
+const router = express.Router(); // 라우터
+const ctrl = require('../controllers/controllers'); // 컨트롤러 모듈
 
-// router.get('/', ctrl.eApi.getTest3);
+router.get('/', ctrl.uApi.getTest); 
+// server.js app.use의 '/users'
 
-// module.exports = router;
+module.exports = router;
