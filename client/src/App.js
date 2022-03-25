fcdc2c2f8951d1885501bb114285eb549e66d64c
eumcloud <livelch012@naver.com>
2022-03-25 10:06:10 +0000

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import './assets/css/App.css';
-import { Routes, Route } from 'react-router-dom';
-import Main from './components/pages/Main';
-import Trade from './components/pages/Trade';
-import Mypage from './components/pages/Mypage';
-import Login from './components/pages/Login';
-import Reference from './components/pages/Reference';
+import "./assets/css/App.css";
+import { Routes, Route } from "react-router-dom";
+import Main from "./components/pages/Main";
+import Trade from "./components/pages/Trade";
+import Mypage from "./components/pages/Mypage";
+import Login from "./components/pages/Login";
+import Join from "./components/pages/Join";
 
 function App() {
   return (
     <>
-        <Routes>
-              <Route path="/" element={<Main/>}/>
-              <Route path="/reference" element={<Reference/>}/>
-              <Route path="/trade" element={<Trade/>}/>
-              <Route path="/mypage" element={<Mypage/>}/>
-              <Route path="/login" element={<Login/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/trade" element={<Trade />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+      </Routes>
     </>
   );
 }
