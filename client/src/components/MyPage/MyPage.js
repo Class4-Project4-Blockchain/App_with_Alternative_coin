cfbe08f68f772a1725727aadc116a1bb4931153d
jungtaesu <93955118+jungtaesu@users.noreply.github.com>
2022-03-30 21:22:45 +0900

    sendtoaddress

--- client/src/components/MyPage/MyPage.js
+++ client/src/components/MyPage/MyPage.js
@@ -10,10 +10,13 @@ import axios from 'axios';
 
 function MyPage() {
 
+  // const [value, SetValue] = useState('');
   const [Result, SetResult] = useState('');
   const [Dbdata, SetDbdata] = useState([]);
   const [Avalue, SetValue] = useState("");
   const [disabled, setDisabled] = useState(false);
+  const [sendAddress, SetAddress] = useState('');
+  const [sendAmount, SetAmount] = useState('');
 
   useEffect(()=>{ //마이페이지에 들어왔을 때 콘솔에 walletinfo 띄움.
     axios.get('http://localhost:3001/mywallet/show').then(({data})=>{
@@ -22,7 +25,7 @@ function MyPage() {
     })
   }, []);
 
-console.log(Dbdata);
+// console.log(Dbdata);
 
     // window.localStorage.setItem('logindata', JSON.stringify(Dbdata));
   // console.log('Dbdata:' + Dbdata);
@@ -30,6 +33,8 @@ console.log(Dbdata);
   // ↓↓↓↓↓↓↓↓↓↓↓ 지갑생성
 
 const handlechange = ({ target: { value }}) => SetValue(value);
+const handlechange2 = ({ target: { value }}) => SetAddress(value);
+const handlechange3 = ({ target: { value }}) => SetAmount(value);
 
 const IDSubmit = async (event) => {
   setDisabled(true);
@@ -58,7 +63,7 @@ const FindWalletSubmit = async (event) => {
 axios.post('http://localhost:3001/mywallet/getaddressesbyaccount/taesu1',{
   account: Avalue
 }).then((result)=>{
-  console.log(result.data.result[0])
+  // console.log(result.data.result[0])
   SetResult(result.data.result[0]); //변경값을 이렇게 설정한 이유는 인풋에서 설정하려고하면 속성을 못읽어서 새로고침하면 값이 날아가버려오류생김.
   
 });
@@ -66,9 +71,24 @@ axios.post('http://localhost:3001/mywallet/getaddressesbyaccount/taesu1',{
   
 };
 
-// console.log(Result);
+// ↓↓↓↓↓↓↓↓↓↓↓ 코인보내기
+const sendToAddressSubmit = async (event) => {
+  setDisabled(true);
+  event.preventDefault();
+  await new Promise((r) => setTimeout(r, 1000));
+  alert(`보낼 지갑의 주소: ${sendAddress}` + `보낼 수량: ${sendAmount}`+"개");
 
-// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑지갑찾기
+  axios.post('http://localhost:3001/mywallet/sendtoaddress', {
+    address: sendAddress,
+    amount: sendAmount
+  }).then((result)=>{
+    console.log(result);
+    SetAddress(result.address);
+    SetAmount(result.amount);
+  });
+  setDisabled(false);
+}
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑ 코인 보내기
 // console.log(Dbdata);
     return (
       <>
@@ -89,19 +109,19 @@ axios.post('http://localhost:3001/mywallet/getaddressesbyaccount/taesu1',{
                 <li>
                   <form onSubmit={IDSubmit}>
                     
-                  <input style={{width : '353px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '12px', borderRadius: '10px'}} type='text' placeholder='원하는 id입력' name="account" value={Avalue} onChange={handlechange}></input>
+                  <input style={{width : '353px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '12px', borderRadius: '10px'}} type='text' placeholder='원하는 id입력' name="account" value={Avalue || ''} onChange={handlechange}></input>
                   <button type="submit" disabled={disabled} style={{marginLeft : '10px', width : '170px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '10px', borderRadius: '10px'}} >지갑생성</button>  
                   </form>
                 </li>
               
                 <li>
                 <form onSubmit={FindWalletSubmit} >
-                  <input placeholder='원하는 id입력' name="account" value={Avalue} onChange={handlechange} style={{width : '353px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '12px', borderRadius: '10px'}}>
+                  <input placeholder='원하는 id입력' name="account" value={Avalue || ''} onChange={handlechange} style={{width : '353px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '12px', borderRadius: '10px'}}>
                   </input>  
                   <button type="submit" disabled={disabled} style={{marginLeft : '10px', width : '170px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '10px', borderRadius: '10px'}} >지갑주소찾기</button>
                   
                   </form>
-                  <input type='text' placeholder='찾는 지갑주소' value={Result} onChange={handlechange} style={{width: '537px', marginBottom : '20px' , border : '1px solid #23518C', padding: '12px', borderRadius: '10px'}}></input> 
+                  <input type='text' placeholder='찾는 지갑주소' value={Result || ''} onChange={handlechange} style={{width: '537px', marginBottom : '20px' , border : '1px solid #23518C', padding: '12px', borderRadius: '10px'}}></input> 
                 </li>            
               </MyPageList>
             </ul>
@@ -109,18 +129,15 @@ axios.post('http://localhost:3001/mywallet/getaddressesbyaccount/taesu1',{
               <MyPageTitle>송금</MyPageTitle>
               <MyPageList>
                 <li>
-                  보낼지갑주소
-                  <form style={{marginBottom : '20px' , border : '1px solid #23518C', padding: '5px', borderRadius: '10px'}}>
-                  <input name="payto" style={{width : '99%', borderWidth: '0', height: '25px', borderRadius: '6px'}}/>                  
-                  </form>           
-                </li>
-                <li>
+                  보낼지갑주소 
+                  <form onSubmit={sendToAddressSubmit} style={{marginBottom : '20px' , border : '1px solid #23518C', padding: '5px', borderRadius: '10px'}}>
+                  <input name="sendAddress" onChange={handlechange2} value={sendAddress || ''} style={{width : '99%', borderWidth: '0', height: '25px', borderRadius: '6px'}}/>                  
                   보내는수량 
-                  <form style={{marginBottom : '20px' , border : '1px solid #23518C', padding: '5px', borderRadius: '10px'}}>
-                  <input name="amount" style={{width : '99%', borderWidth: '0', height: '25px', borderRadius: '6px'}} placeholder='0.00000000'/>              
+                  <input name="sendAmount" onChange={handlechange3} value={sendAmount || ''} style={{width : '99%', borderWidth: '0', height: '25px', borderRadius: '6px'}} placeholder='0.00000000'/> 
+                  <button style={{width : '400px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '10px', borderRadius: '10px'}} type="submit">보내기</button>              
                   </form>  
                 </li>
-                <button style={{width : '561px' ,marginBottom : '20px' , border : '1px solid #23518C', padding: '10px', borderRadius: '10px'}} type="submit">보내기</button>                      
+                     
               </MyPageList>
             </ul>
             <ul>
